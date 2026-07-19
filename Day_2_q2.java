a164586c356d7977d56149296fcddf2f468a9060
SimoneG-Coder <simone22012006@gmail.com>
2026-07-19 14:27:39 +0530

    Update Day_2_q2.java

--- Day_2_q2.java
+++ Day_2_q2.java
@@ -3,31 +3,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        if (!sc.hasNextLine()) {
-            System.out.println(0);
-            return;
-        }
-        
-        // Appending a space forces the last character group to trigger the 'else' block
-        String s = sc.nextLine() + " "; 
-        
-        int totalSum = 0;
+        String s = sc.nextLine();
+
+        int totalEvenSum = 0;
+        int runningSum = 0;
         int count = 1;
 
-        // Start from 1 because a single character is a run of length 1
         for (int i = 1; i < s.length(); i++) {
             if (s.charAt(i) == s.charAt(i - 1)) {
-                count++; // Streak continues
+                count++;
             } else {
-                // Streak ended! Check if the group length we just finished is even
+                // check the run we just finished
                 if (count % 2 == 0) {
-                    totalSum += count;
+                    runningSum += count;
                 }
-                count = 1; // Reset count to 1 for the new character streak
+                count = 1; // reset for new run
             }
         }
 
-        System.out.println(totalSum);
+        // handle last run
+        if (count % 2 == 0) {
+            runningSum += count;
+        }
+
+        totalEvenSum = Math.max(totalEvenSum, runningSum);
+
+        System.out.println(totalEvenSum);
     }
 }
