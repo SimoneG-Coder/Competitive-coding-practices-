e416770843152ea3a3c28bd148cd007d7a568db5
SimoneG-Coder <simone22012006@gmail.com>
2026-07-28 09:50:41 +0530

    Update Day_5_q3.java

--- Day_5_q3.java
+++ Day_5_q3.java
@@ -1,9 +1,8 @@
 import java.util.*;
 
-public class BinaryPalindrome {
-    // Function to check if a number is a binary palindrome
+public class BinaryPalindromeString {
     public static boolean isBinaryPalindrome(int n) {
-        String binary = Integer.toBinaryString(n); // convert to binary string
+        String binary = Integer.toBinaryString(n);
         int i = 0, j = binary.length() - 1;
         while (i < j) {
             if (binary.charAt(i) != binary.charAt(j)) {
@@ -17,13 +16,16 @@ public class BinaryPalindrome {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a decimal number: ");
         int num = sc.nextInt();
 
+        String binary = Integer.toBinaryString(num);
+        System.out.println("Binary representation: " + binary);
+
         if (isBinaryPalindrome(num)) {
-            System.out.println(num + " is a binary palindrome.");
+            System.out.println("Palindrome (String method).");
         } else {
-            System.out.println(num + " is NOT a binary palindrome.");
+            System.out.println("Not Palindrome (String method).");
         }
 
         sc.close();
@@ -34,14 +36,12 @@ public class BinaryPalindrome {
 import java.util.*;
 
 public class BinaryPalindromeBitwise {
-    // Function to check if n is a binary palindrome using bit operators
     public static boolean isBinaryPalindrome(int n) {
-        int left = 31; // start from highest bit (for int)
+        int left = 31; // highest bit position for int
         while (((n >> left) & 1) == 0 && left > 0) {
             left--; // skip leading zeros
         }
-        int right = 0; // start from lowest bit
-
+        int right = 0;
         while (right < left) {
             int leftBit = (n >> left) & 1;
             int rightBit = (n >> right) & 1;
@@ -56,13 +56,16 @@ public class BinaryPalindromeBitwise {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a decimal number: ");
         int num = sc.nextInt();
 
+        String binary = Integer.toBinaryString(num);
+        System.out.println("Binary representation: " + binary);
+
         if (isBinaryPalindrome(num)) {
-            System.out.println(num + " is a binary palindrome.");
+            System.out.println("Palindrome (Bitwise method).");
         } else {
-            System.out.println(num + " is NOT a binary palindrome.");
+            System.out.println("Not Palindrome (Bitwise method).");
         }
 
         sc.close();
