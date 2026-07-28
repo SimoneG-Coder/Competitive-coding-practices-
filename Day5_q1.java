b40d7577690e95fd74f65d1f7d9d6d867ff66a01
SimoneG-Coder <simone22012006@gmail.com>
2026-07-28 08:58:46 +0530

    Create Day5_q1.java

---
+++ Day5_q1.java
@@ -0,0 +1,7 @@
+public class ToggleSwitch {
+    public static void main(String[] args) {
+        int n = 100;
+        int openDoors = (int) Math.sqrt(n);
+        System.out.println("Open doors = " + openDoors);
+    }
+}
