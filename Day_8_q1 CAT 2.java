a5ba133e9bf5e73d5ab14595ecb35b2a110dd13c
SimoneG-Coder <231528174+SimoneG-Coder@users.noreply.github.com>
2026-08-27 09:04:33 +0530

    Update Day_8_q1 CAT 2.java

--- Day_8_q1 CAT 2.java
+++ Day_8_q1 CAT 2.java
@@ -1,1 +1,24 @@
+//Move Hyphen To The Beginning
+public class MoveHyphen {
+    public static void main(String[] args) {
+        String input = "ab-cd-e-fg--h";
+        String result = moveHyphensToFront(input);
+        System.out.println("Original: " + input);
+        System.out.println("Modified: " + result);
+    }
 
+    public static String moveHyphensToFront(String str) {
+        StringBuilder hyphens = new StringBuilder();
+        StringBuilder others = new StringBuilder();
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '-') {
+                hyphens.append(ch);
+            } else {
+                others.append(ch);
+            }
+        }
+
+        return hyphens.append(others).toString();
+    }
+}
